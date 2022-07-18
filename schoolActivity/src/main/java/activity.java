94d1a7bd87371161ece1eba53e132bc955d0b091
nebrida <nebrida02@gmail.com>
2022-07-19 01:37:06 +0800

    Created new project

---
+++ schoolActivity/src/main/java/activity.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class activity {
+    public static void main (String []args){
+    
+    Scanner sc = new Scanner(System.in);
+    
+        System.out.println("First name : ");
+        String fname = sc.nextLine();
+        
+        System.out.println("Last name : ");
+        String lname = sc.nextLine();
+        
+        System.out.println("Address : ");
+        String add = sc.nextLine();
+
+        System.out.println("Name : " + fname + lname);
+        System.out.println("Address : " + add);
+        
+    
+    }
+    
+}
